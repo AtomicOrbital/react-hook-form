b537ceb4bd8d1e12fb378095358ca59763a320d4
Nguyen Thanh Dat <topthuthuat69@gmail.com>
2023-04-19 17:47:16 +0700

    signup

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-export const APIHost = development ? '/api' : 'https://google.com';
+export const APIHost = development ? '/api' : '/api';
 
 // export const APIHost = development ? '/api/v1/location'
 
